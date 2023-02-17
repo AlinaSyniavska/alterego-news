bfb6be159949f2bf631bbd71a4f5e6d78752af29
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-18 00:41:29 +0200

    adding HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,30 @@
 import {FC} from "react";
+import React from "react";
+import {Container} from "@mui/material";
 
 const HomePage: FC = () => {
     return (
-        <div>
-            HomePage
-        </div>
+        <React.Fragment>
+            <Container maxWidth="lg">
+                <h1>Web App with news</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque enim inventore, iste itaque
+                    necessitatibus placeat provident quam recusandae rem totam! Aperiam atque, blanditiis hic natus
+                    officiis possimus qui. Dolore minima molestias, ratione sit tenetur unde vel? Architecto consectetur
+                    consequatur delectus distinctio enim eum illum impedit in quaerat recusandae, reiciendis sapiente
+                    sunt tempore, tenetur voluptas. Aliquam, assumenda consectetur, cum delectus ducimus excepturi
+                    explicabo facilis iure neque, numquam odio pariatur placeat provident quasi sequi sunt vitae?
+                    Asperiores at error est, excepturi, facere necessitatibus officiis possimus quam quibusdam quidem
+                    quos ratione recusandae, reprehenderit repudiandae tempore! A ad autem dolore dolorem, ea, enim eos
+                    est eum hic ipsum minima numquam quas repellendus reprehenderit vitae. Ad consequuntur fuga incidunt
+                    magni minus, mollitia necessitatibus officia quae quam repudiandae rerum sint vel vitae. Accusamus
+                    aliquid aperiam atque beatae corporis cum distinctio dolore dolorum, error esse explicabo facilis
+                    fugit ipsam iure laborum, laudantium libero magnam, minus nobis nulla numquam optio provident quos
+                    saepe sapiente tempora voluptatem? Alias architecto aspernatur, atque consequuntur debitis, dolores
+                    doloribus eius libero maiores molestiae neque optio possimus quam ratione velit! At eius quibusdam
+                    quidem quo unde! Ad adipisci aliquid, assumenda, cum esse iste magni nihil, non possimus quis quo
+                    quod suscipit! Consequuntur, fugiat, quisquam!</p>
+            </Container>
+        </React.Fragment>
     );
 };
 
