eef4f5a2196e8c19625694222046c8913511f974
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-19 22:03:36 +0200

    adding fake credentials

---
+++ src/constants/credentials.ts
@@ -0,0 +1,8 @@
+const fakeCredentials = {
+    username: 'admin',
+    password: '12345',
+};
+
+export {
+    fakeCredentials,
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
+export  * from './credentials';
 export  * from './newsConst';
 export  * from './urls';
