679856867c2117de3a75207d62a01dfc8867115a
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-20 16:05:58 +0200

    adding ProfilePage

--- src/constants/credentials.ts
+++ src/constants/credentials.ts
@@ -1,6 +1,7 @@
 const fakeCredentials = {
     USERNAME: 'admin',
     PASSWORD: '12345',
+    AVATAR_URL: 'src/images/user-avatar.webp'
 };
 
 export {
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,27 @@
 import {FC} from "react";
+import React from "react";
+import {Avatar, Stack} from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+import {authService} from "../../services";
+import {localStorageItems} from "../../constants";
 
 const ProfilePage: FC = () => {
+    const authUser = authService.getAuthUser(localStorageItems.LOGIN_USER);
+
     return (
-        <div>
-            ProfilePage
-        </div>
+        <React.Fragment>
+            <Stack direction="row" spacing={2} sx={{marginTop: '50px'}}>
+                <Avatar
+                    alt={`${authUser.username}`}
+                    src="https://images.pexels.com/photos/4016173/pexels-photo-4016173.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=200"
+                    sx={{ width: 55, height: 55 }}
+                />
+                <Typography variant="h3" component="h3">
+                    {authUser.username}
+                </Typography>
+            </Stack>
+        </React.Fragment>
     );
 };
 
