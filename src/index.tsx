a89bfc39d3e8e3eefd2e92064b6b7f1f1c4696b3
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-20 23:58:46 +0200

    react-i18next Add internationalization

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,17 @@ import {setupStore} from "./redux";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+
 import {App} from './App';
 
+i18next.init({
+    interpolation: {escapeValue: false},  // React already does escaping
+});
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 const store = setupStore();
@@ -15,7 +22,9 @@ const store = setupStore();
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <I18nextProvider i18n={i18next}>
+                <App/>
+            </I18nextProvider>
         </BrowserRouter>
     </Provider>
 );
