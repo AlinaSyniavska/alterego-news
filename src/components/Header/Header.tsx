6f32b36b57694fd703179347ec31f4658d659c98
AlinaSyniavska <alina22syniavska@gmail.com>
2023-02-21 23:07:12 +0200

    adding buttons select language

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,6 +7,26 @@ import style from './Header.module.css';
 const Header: FC = () => {
     const {t, i18n} = useTranslation('common');
 
+    const selectENLanguage = () => {
+        const btnEngLanguage = document.getElementById('eng');
+        const btnUkrLanguage = document.getElementById('ukr');
+
+        btnUkrLanguage && btnUkrLanguage.classList.remove('select');
+        btnEngLanguage && btnEngLanguage.classList.add('select');
+
+        i18n.changeLanguage('en');
+    }
+
+    const selectUKLanguage = () => {
+        const btnEngLanguage = document.getElementById('eng');
+        const btnUkrLanguage = document.getElementById('ukr');
+
+        btnEngLanguage && btnEngLanguage.classList.remove('select');
+        btnUkrLanguage && btnUkrLanguage.classList.add('select');
+
+        i18n.changeLanguage('uk');
+    }
+
     return (
         <div className={style.headerContainer}>
             <NavLink to='/home'
@@ -27,6 +47,11 @@ const Header: FC = () => {
                      })}>
                 {t('headerItems.profile')}
             </NavLink>
+
+            <div className={style.btnSelectLanguage}>
+                <button id={'eng'} onClick={selectENLanguage}>EN</button>
+                <button id={'ukr'} onClick={selectUKLanguage}>UK</button>
+            </div>
         </div>
     );
 };
